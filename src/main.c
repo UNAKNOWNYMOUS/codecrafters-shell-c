f86b01dd3cff89175141f458cd3c00d56d4acf73
UNAKNOWNYMOUS <UNAKNOWNYMOUS@protonmail.com>
2026-06-02 00:53:07 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[]) {
   }
 
   if (read != -1) {
-    printf("%s: command not found", command);
+    // printf("%s: command not found", command);
   }
 
   free(command);
