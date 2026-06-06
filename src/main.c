621c33a6a06841783ef329f28501c36fd7fda46d
UNAKNOWNYMOUS <UNAKNOWNYMOUS@protonmail.com>
2026-06-05 22:38:16 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -23,6 +23,8 @@ int main(int argc, char *argv[]) {
 
     if (strcmp(command, "exit") == 0) {
       break;
+    } else if (strncmp(command, "echo", 5) == 0) {
+      printf("%s\n", &command[5]);
     } else {
       printf("%s: command not found\n", command);
     }
